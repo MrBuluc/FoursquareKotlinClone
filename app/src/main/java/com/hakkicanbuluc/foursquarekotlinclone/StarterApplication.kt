959d74277f8365a2e7d58e9d66058ee1c7bd8c49
HAKKICAN <hakkicanbuluc@gmail.com>
2021-08-08 17:18:52 +0300

    Finish the project

--- app/src/main/java/com/hakkicanbuluc/foursquarekotlinclone/StarterApplication.kt
+++ app/src/main/java/com/hakkicanbuluc/foursquarekotlinclone/StarterApplication.kt
@@ -2,6 +2,8 @@ package com.hakkicanbuluc.foursquarekotlinclone
 
 import android.app.Application
 import com.parse.Parse
+import com.parse.ParseACL
+import com.parse.ParseUser
 
 class StarterApplication : Application() {
 
@@ -11,9 +13,9 @@ class StarterApplication : Application() {
         Parse.setLogLevel(Parse.LOG_LEVEL_DEBUG)
 
         Parse.initialize(Parse.Configuration.Builder(this)
-            .applicationId("")
-            .clientKey("")
-            .server("")
+            .applicationId("SgbqlZGoaxGy7ZauBwFnvuWMrEOnMPDYjsSTI2jL")
+            .clientKey("TXCMkMYl742Lrh2Os8WSdIdukJdyNFrYfbIdLwD6")
+            .server("https://parseapi.back4app.com/")
             .build())
     }
 }
